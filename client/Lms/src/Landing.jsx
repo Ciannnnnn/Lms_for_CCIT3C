2e29b2dd827656aa713e0fa8ca41e57aa8ad754d
John Cian Lioc <johncianlioc01@gmail.com>
2025-11-02 19:34:48 +0800

    add logo

--- client/Lms/src/Landing.jsx
+++ client/Lms/src/Landing.jsx
@@ -3,6 +3,8 @@ import Login_Prof from "./Login_Prof";
 import Prof_Upload from "./Prof_Upload";
 import Login from "./Login";
 import './Landing.css';
+import prmsuLogo from './assets/PRMSU.png';
+import ccitLogo from './assets/ccit.png';
 
 function Landing() {
   const navigate = useNavigate();
@@ -14,7 +16,14 @@ function Landing() {
         path="/"
         element={
 
-          <div id="box"><br />
+          <div id="box">
+            <div id="picHolder">
+
+              <img src={prmsuLogo} alt="prmsu logo" id="prmsuLogo"/>
+              <img src={ccitLogo} alt="ccit logo" id="ccitLogo"/>
+
+            </div>
+            <br />
             <button id="prof" onClick={() => navigate("/Login_Prof")}>Professor</button><br />
             <button id="student" onClick={() => navigate("Login")}>Student</button>
           </div>
